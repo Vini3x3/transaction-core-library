3be0b1c041eb811af317b3cbd87e03d98916fc51
Vini3x3 <vincent.hanghang@gmail.com>
2024-06-26 21:42:32 +0800

    add tag

--- src/main/java/com/transactionHub/transactionCoreLibrary/constant/TagType.java
+++ src/main/java/com/transactionHub/transactionCoreLibrary/constant/TagType.java
@@ -1,8 +1,13 @@
 package com.transactionHub.transactionCoreLibrary.constant;
 
 public class TagType {
+
+    // category
     public static final String SCHEDULE = "SCHEDULE";
     public static final String INVEST = "INVEST";
+    public static final String WALLET = "WALLET";
+
+    // utility
     public static final String FILE = "FILE";
     public static final String VIRTUAL = "VIRTUAL";
     public static final String SYS = "SYS";
