33d682b69459a337ca5eafe44ae20c4eebe68447
Vini3x3 <vincent.hanghang@gmail.com>
2024-06-29 16:25:13 +0800

    migrate constant

--- src/main/java/com/transactionHub/transactionCoreLibrary/constant/TagConstant.java
+++ src/main/java/com/transactionHub/transactionCoreLibrary/constant/TagConstant.java
@@ -1,7 +1,6 @@
 package com.transactionHub.transactionCoreLibrary.constant;
 
 public class TagConstant {
-    public static final String FILE = "FILE";
     public static final String VIRTUAL = "VIRTUAL";
     public static final String SYS = "SYS";
 }
--- src/main/java/com/transactionHub/transactionCoreLibrary/constant/TagType.java
+++ src/main/java/com/transactionHub/transactionCoreLibrary/constant/TagType.java
@@ -4,4 +4,7 @@ public class TagType {
     public static final String SCHEDULE = "SCHEDULE";
     public static final String INVEST = "INVEST";
     public static final String WALLET = "WALLET";
+    public static final String TRANSFER = "TRANSFER";
+    public static final String FILE = "FILE";
+    public static final String LINK = "LINK";
 }
